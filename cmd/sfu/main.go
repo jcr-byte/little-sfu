be7a3dddcb553450f10594c44097771211dfb05b
joshua rodli <joshuarodli19@gmail.com>
2026-08-31 10:33:13 -0700

    connect the browser publisher to the signaling endpoint

--- cmd/sfu/main.go
+++ cmd/sfu/main.go
@@ -3,21 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/josh/little-sfu/internal/signaling"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
-		}
+	server := signaling.NewServer()
 
+	serveMux := http.NewServeMux()
+
+	serveMux.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "internal/web/index.html")
 	})
 
+	serveMux.HandleFunc("POST /publish/{room}", server.PublishHandler)
+
 	log.Println("Little SFU listening on http://localhost:8080")
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", serveMux); err != nil {
 		log.Fatal(err)
 	}
 }
